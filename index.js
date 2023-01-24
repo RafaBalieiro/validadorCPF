b86bf71448bc522173c773b58c07d97ec8f02eec
Rafael Barros <rafaelbarrosbalieiro20@gmail.com>
2023-01-24 01:13:07 -0300

    Exercicio de polimorfismo

---
+++ index.js
@@ -0,0 +1,66 @@
+//Execício Polimorfismo
+
+function Conta(agencia, conta, saldo) {
+  this.agencia = agencia;
+  this.conta = conta;
+  this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function (valor) {
+  if (valor > this.saldo) {
+    console.log(`Saldo insuficiente: ${this.saldo}`);
+    return;
+  }
+
+  this.saldo -= valor;
+  this.verSaldo();
+};
+
+Conta.prototype.depositar = function (valor) {
+  this.saldo += valor;
+  this.verSaldo();
+};
+
+Conta.prototype.verSaldo = function () {
+  console.log(
+    `Agencia: ${this.agencia}/${this.conta} | Saldo: R$${this.saldo.toFixed(2)}`
+  );
+};
+
+function ContaCorrente(agencia, conta, saldo, limite) {
+  Conta.call(this, agencia, conta, saldo);
+  this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function (valor) {
+  if (valor > this.saldo + this.limite) {
+    console.log(`Saldo insuficiente: ${this.saldo}`);
+    return;
+  }
+
+  this.saldo -= valor;
+  this.verSaldo();
+};
+
+function ContaPoupanca(agencia, conta, saldo) {
+  Conta.call(this, agencia, conta, saldo);
+}
+
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.constructor = ContaPoupanca;
+
+const ContaCorrente1 = new ContaCorrente(11, 22, 0, 100);
+ContaCorrente1.depositar(10);
+ContaCorrente1.sacar(110);
+ContaCorrente1.sacar(1);
+
+console.log();
+
+const ContaPoupanca1 = new ContaPoupanca(12, 33, 0);
+
+ContaPoupanca1.depositar(10);
+ContaPoupanca1.sacar(10);
+ContaPoupanca1.sacar(1);
